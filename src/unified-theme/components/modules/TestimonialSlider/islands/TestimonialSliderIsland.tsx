7d8e85a8b24e912dfd0c1b3302ce244b531f9e44
Kamil <kamil.kowalczyk@bang.fi>
2026-03-11 14:27:14 +0200

    padding and min-height added to slide while info layout chosen

--- src/unified-theme/components/modules/TestimonialSlider/islands/TestimonialSliderIsland.tsx
+++ src/unified-theme/components/modules/TestimonialSlider/islands/TestimonialSliderIsland.tsx
@@ -381,8 +381,9 @@ const TestimonialSlider = (props: TestimonialSliderProps) => {
           <div className="splide__list hs-elevate-testimonial-slider__list">
             {groupTestimonial.map((testimonial, index) => {
               const infoImage = layoutType === 'info' ? testimonial.groupInfoImage?.image : undefined;
+              const hasInfoBackground = Boolean(infoImage?.src);
               const slideStyle =
-                layoutType === 'info' && infoImage?.src
+                layoutType === 'info' && hasInfoBackground
                   ? {
                       backgroundImage: `url(${infoImage.src})`,
                       backgroundSize: 'cover',
@@ -392,7 +393,9 @@ const TestimonialSlider = (props: TestimonialSliderProps) => {
 
               return (
                 <div
-                  className="splide__slide hs-elevate-testimonial-slider__slide"
+                  className={cx('splide__slide', 'hs-elevate-testimonial-slider__slide', {
+                    [styles['hs-elevate-testimonial-slider__slide--info-has-bg']]: layoutType === 'info' && hasInfoBackground,
+                  })}
                   style={slideStyle}
                   key={layoutType === 'info' ? index : testimonial.groupQuote.quote}
                 >
