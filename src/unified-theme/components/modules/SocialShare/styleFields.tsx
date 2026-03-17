fd3a87381c1b48b09c156db95c7ba4aa674dca9f
Kamil <kamil.kowalczyk@bang.fi>
2026-03-17 11:58:42 +0200

    social icons - border can be switched off

--- src/unified-theme/components/modules/SocialShare/styleFields.tsx
+++ src/unified-theme/components/modules/SocialShare/styleFields.tsx
@@ -24,7 +24,6 @@ export default function StyleFields() {
           ['default', 'Default'],
           ['none', 'None'],
         ]}
-        display="buttons"
         required={true}
         default="default"
       />
