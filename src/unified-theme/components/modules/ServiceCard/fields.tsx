47b650af77356c8757f0798054d136adb1861059
Kamil <kamil.kowalczyk@bang.fi>
2026-03-25 11:24:30 +0200

    Updated ServiceCard to be truly independent of Card on the HubSpot Designer canvas, without touching Card or any imported shared components All requested ServiceCard independence changes are complete (CSS selector rename + CSS filename rename + import update), and the theme builds successfully

---
+++ src/unified-theme/components/modules/ServiceCard/fields.tsx
@@ -0,0 +1,183 @@
+import {
+  ModuleFields,
+  RepeatedFieldGroup,
+  IconField,
+  ChoiceField,
+  ImageField,
+  BooleanField,
+  FieldGroup,
+  AdvancedVisibility,
+  TextField,
+  Visibility,
+} from '@hubspot/cms-components/fields';
+import {
+  HeadingAndText,
+  ButtonContent,
+  RichTextContent,
+} from '../../fieldLibrary/index.js';
+import StyleFields from './styleFields.js';
+import newsletterImage from './assets/newsletter.png';
+
+const buttonFieldVisibility: AdvancedVisibility = {
+  boolean_operator: 'OR',
+  criteria: [{
+    controlling_field_path: 'groupCards.groupButton.showButton',
+    controlling_value_regex: 'true',
+    operator: 'EQUAL',
+  }],
+} as const;
+
+const captionFieldsVisibility = {
+  controlling_field_path: 'groupCards.groupContent.showCaption',
+  controlling_value_regex: 'true',
+  operator: 'EQUAL',
+} as const satisfies Visibility;
+
+export const fields = (
+  <ModuleFields>
+    <ChoiceField
+      label='Image or icon'
+      name='imageOrIcon'
+      choices={[
+        ['image', 'Image'],
+        ['icon', 'Icon'],
+      ]}
+      default='icon'
+    />
+    <RepeatedFieldGroup
+      label='Cards'
+      name='groupCards'
+      id='groupCards'
+      occurrence={{
+        min: 1,
+        max: 4,
+        sorting_label_field: 'groupCards.groupContent',
+        default: 1,
+      }}
+      default={[
+        {
+          groupIcon: {
+            icon: {
+              name: 'play-circle',
+            },
+          },
+          groupImage: {
+            showRoundImageBorder: false,
+            image: {
+              src: newsletterImage,
+              alt: '',
+              loading: 'lazy',
+            },
+          },
+          groupContent: {
+            showCaption: false,
+            captionText: '',
+            headingAndTextHeadingLevel: 'h3',
+            headingAndTextHeading: 'Content Creation',
+            richTextContentHTML:
+              "<p>Stand out with our captivating content creation services, tailored to engage today's digital audience </p>",
+          },
+          groupButton: {
+            showButton: false,
+            buttonContentText: 'Explore more',
+            buttonContentLink: {
+              open_in_new_tab: true,
+            },
+            buttonContentShowIcon: false,
+            buttonContentIcon: {
+              name: 'arrow-right',
+            },
+            buttonContentIconPosition: 'right',
+          },
+        },
+      ]}
+    >
+      <FieldGroup label='Icon' name='groupIcon' display='inline'>
+        <IconField
+          label='Icon'
+          name='icon'
+          iconSet='fontawesome-6.4.2'
+          visibility={{
+            controlling_field: 'imageOrIcon',
+            controlling_value_regex: 'icon',
+            operator: 'EQUAL',
+          }}
+          default={{
+            name: 'wrench',
+          }}
+        />
+      </FieldGroup>
+      <FieldGroup label='Image' name='groupImage' display='inline'>
+        <ImageField
+          label='Image'
+          name='image'
+          visibility={{
+            controlling_field: 'imageOrIcon',
+            controlling_value_regex: 'image',
+            operator: 'EQUAL',
+          }}
+          resizable={false}
+          responsive={false}
+          showLoading={true}
+          default={{
+            alt: '',
+            loading: 'lazy',
+            src: newsletterImage,
+          }}
+          inlineEditable={true}
+        />
+        <BooleanField
+          label='Round border around image'
+          name='showRoundImageBorder'
+          display='toggle'
+          default={false}
+          visibility={{
+            controlling_field: 'imageOrIcon',
+            controlling_value_regex: 'image',
+            operator: 'EQUAL',
+          }}
+        />
+      </FieldGroup>
+      <FieldGroup label='Content' name='groupContent' display='inline' id='groupContent'>
+        <BooleanField label='Show caption' name='showCaption' display='toggle' default={false} />
+        <TextField
+          label='Caption'
+          name='captionText'
+          visibility={captionFieldsVisibility}
+          default=''
+          inlineEditable={true}
+        />
+        <HeadingAndText headingTextLabel='Title' headingLevelDefault='h3' textDefault='Content Creation' />
+        <RichTextContent
+          label='Description'
+          richTextDefault="<p>Stand out with our captivating content creation services, tailored to engage today's digital audience</p>"
+          featureSet='text'
+        />
+      </FieldGroup>
+      <FieldGroup label='Button' name='groupButton' display='inline'>
+        <BooleanField label='Show button' name='showButton' display='toggle' default={false} />
+        <ButtonContent
+          textDefault='Explore more'
+          linkDefault={{
+            open_in_new_tab: true,
+          }}
+          textVisibility={buttonFieldVisibility}
+          linkVisibility={buttonFieldVisibility}
+          showIconVisibility={buttonFieldVisibility}
+        />
+      </FieldGroup>
+      <FieldGroup label='Card background' name='groupCardBackground' display='inline'>
+        <ImageField
+          label='Card background image'
+          name='image'
+          resizable={false}
+          responsive={false}
+          showLoading={true}
+          inlineEditable={true}
+        />
+      </FieldGroup>
+    </RepeatedFieldGroup>
+    <StyleFields />
+  </ModuleFields>
+);
+
---
+++ src/unified-theme/components/modules/ServiceCard/index.tsx
@@ -0,0 +1,362 @@
+import { ModuleMeta } from '../../types/modules.js';
+import { Icon, RichText } from '@hubspot/cms-components';
+import {
+  AlignmentFieldType,
+  BooleanFieldType,
+  IconFieldType,
+  ImageFieldType,
+  TextFieldType,
+} from '@hubspot/cms-components/fields';
+import { ButtonStyleType, StandardSizeType } from '../../types/fields.js';
+import cardIconSvg from './assets/card-icon-temp.svg';
+import { getAlignmentFieldCss } from '../../utils/style-fields.js';
+import { getLinkFieldHref, getLinkFieldRel, getLinkFieldTarget } from '../../utils/content-fields.js';
+import { Card } from '../../CardComponent/index.js';
+import HeadingComponent from '../../HeadingComponent/index.js';
+import styles from './service-card.module.css';
+import { RichTextContentFieldLibraryType } from '../../fieldLibrary/RichTextContent/types.js';
+import { Button } from '../../ButtonComponent/index.js';
+import { ButtonContentType } from '../../fieldLibrary/ButtonContent/types.js';
+import { HeadingStyleFieldLibraryType } from '../../fieldLibrary/HeadingStyle/types.js';
+import { HeadingAndTextFieldLibraryType } from '../../fieldLibrary/HeadingAndText/types.js';
+import { CardStyleFieldLibraryType } from '../../fieldLibrary/CardStyle/types.js';
+import cx, { staticWithModule } from '../../utils/classnames.js';
+import { createComponent } from '../../utils/create-component.js';
+import { CSSPropertiesMap } from '../../types/components.js';
+import { getDataHSToken } from '../../utils/inline-editing.js';
+
+const swm = staticWithModule(styles);
+
+// Types
+
+type IconGroup = {
+  groupIcon: {
+    icon: IconFieldType['default'];
+  };
+};
+
+type ImageGroup = {
+  groupImage: {
+    image: ImageFieldType['default'];
+    showRoundImageBorder?: BooleanFieldType['default'];
+  };
+};
+
+type ContentGroup = {
+  groupContent: RichTextContentFieldLibraryType &
+    HeadingAndTextFieldLibraryType & {
+      showCaption?: BooleanFieldType['default'];
+      captionText?: TextFieldType['default'];
+    };
+};
+
+type ButtonGroup = {
+  groupButton: ButtonContentType & {
+    showButton: BooleanFieldType['default'];
+  };
+};
+
+type CardBackgroundGroup = {
+  groupCardBackground: {
+    image: ImageFieldType['default'];
+  };
+};
+
+type GroupCards = IconGroup & ImageGroup & ContentGroup & ButtonGroup & Partial<CardBackgroundGroup>;
+
+type GroupCardStyles = {
+  groupCard: CardStyleFieldLibraryType & {
+    cardOrientation: 'row' | 'column';
+    showIconBorder?: BooleanFieldType['default'];
+    showCardShadow?: BooleanFieldType['default'];
+    showCardBorder?: BooleanFieldType['default'];
+  };
+};
+
+type GroupContentStyles = {
+  groupContent: HeadingStyleFieldLibraryType & {
+    alignment: AlignmentFieldType['default'];
+    headingUppercase?: BooleanFieldType['default'];
+  };
+};
+
+type GroupButtonStyles = {
+  groupButton: {
+    buttonStyleSize: StandardSizeType;
+    buttonStyleVariant: ButtonStyleType;
+  };
+};
+
+type GroupStyle = GroupCardStyles & GroupContentStyles & GroupButtonStyles;
+
+type ServiceCardProps = {
+  moduleName?: string;
+  imageOrIcon: 'icon' | 'image';
+  groupCards: GroupCards[];
+  groupStyle: GroupStyle;
+  hublData: {
+    renderedWithGrids: boolean;
+  };
+};
+
+// Functions to generate CSS variables
+
+function generateAlignmentCssVars(alignment: AlignmentFieldType['default']): CSSPropertiesMap {
+  const textAlignment = alignment.horizontal_align?.toLowerCase() as 'left' | 'right' | 'center';
+  return {
+    '--hsElevate--card__alignment': getAlignmentFieldCss(alignment).justifyContent,
+    '--hsElevate--card__textAlignment': textAlignment,
+  };
+}
+
+function generateColorCssVars(cardVariantField: string): CSSPropertiesMap {
+  const iconColorsMap = {
+    card_variant_1: {
+      borderRadius: 'var(--hsElevate--card--variant1__iconBorderRadius)',
+      fillColor: 'var(--hsElevate--card--variant1__iconColor)',
+      backgroundColor: 'var(--hsElevate--card--variant1__iconBackgroundColor)',
+    },
+    card_variant_2: {
+      borderRadius: 'var(--hsElevate--card--variant2__iconBorderRadius)',
+      fillColor: 'var(--hsElevate--card--variant2__iconColor)',
+      backgroundColor: 'var(--hsElevate--card--variant2__iconBackgroundColor)',
+    },
+    card_variant_3: {
+      borderRadius: 'var(--hsElevate--card--variant3__iconBorderRadius)',
+      fillColor: 'var(--hsElevate--card--variant3__iconColor)',
+      backgroundColor: 'var(--hsElevate--card--variant3__iconBackgroundColor)',
+    },
+    card_variant_4: {
+      borderRadius: 'var(--hsElevate--card--variant4__iconBorderRadius)',
+      fillColor: 'var(--hsElevate--card--variant4__iconColor)',
+      backgroundColor: 'var(--hsElevate--card--variant4__iconBackgroundColor)',
+    },
+  };
+
+  return {
+    '--hsElevate--cardIcon__borderRadius': iconColorsMap[cardVariantField].borderRadius,
+    '--hsElevate--cardIcon__fillColor': iconColorsMap[cardVariantField].fillColor,
+    '--hsElevate--cardIcon__backgroundColor': iconColorsMap[cardVariantField].backgroundColor,
+  };
+}
+
+// Checks if image path has '-use-background-' in its name to get the card icon's background color applied
+
+function imageShouldUseBackground(imagePath: string): boolean {
+  if (!imagePath) {
+    return false;
+  }
+  return /-use-background-/.test(imagePath);
+}
+
+function getCardBackgroundImageSrc(image: Partial<ImageFieldType['default']> | undefined): string | undefined {
+  const src = image?.src;
+  if (typeof src !== 'string' || !src.trim()) {
+    return undefined;
+  }
+  return src;
+}
+
+// Components
+
+const CardContainer = createComponent('div');
+const IconWrapper = createComponent('div');
+const ImageWrapper = createComponent('div');
+const Image = createComponent('img');
+const CardContent = createComponent('div');
+const ButtonWrapper = createComponent('div');
+const Caption = createComponent('p');
+
+export const Component = (props: ServiceCardProps) => {
+  const {
+    moduleName,
+    imageOrIcon,
+    groupCards,
+    groupStyle: {
+      groupCard: {
+        cardStyleVariant,
+        cardOrientation,
+        showIconBorder = true,
+        showCardShadow = false,
+        showCardBorder = true,
+      },
+      groupContent: { alignment, headingStyleVariant, headingUppercase = false },
+      groupButton: { buttonStyleVariant, buttonStyleSize },
+    },
+    hublData: { renderedWithGrids = false },
+  } = props;
+
+  const isIcon = imageOrIcon === 'icon';
+  const isImage = imageOrIcon === 'image';
+  const textAlignment = alignment.horizontal_align?.toLowerCase() as 'left' | 'right' | 'center';
+
+  const headingInlineStyles = { textAlign: textAlignment };
+
+  const cssVarsMap = {
+    ...generateColorCssVars(cardStyleVariant),
+    ...generateAlignmentCssVars(alignment),
+  };
+
+  const layoutClass = renderedWithGrids
+    ? 'hs-elevate-service-card-container--grids'
+    : 'hs-elevate-service-card-container--bootstrap';
+
+  return (
+    <CardContainer
+      className={cx(swm('hs-elevate-service-card-container'), styles[layoutClass])}
+      style={cssVarsMap}
+    >
+      {groupCards.map((card, index) => {
+        const {
+          groupButton: {
+            showButton,
+            buttonContentText: text,
+            buttonContentLink: link = {},
+            buttonContentShowIcon: showIcon,
+            buttonContentIconPosition: iconPosition,
+          },
+        } = card;
+
+        const hasValidImageSrc = card?.groupImage?.image?.src;
+        const isCardImageWithBackground = hasValidImageSrc && imageShouldUseBackground(card.groupImage.image.src);
+        const cardImageUsesBackground = isImage && isCardImageWithBackground;
+        const isImageVisible = isImage && hasValidImageSrc;
+        const hasValidIconName = card?.groupIcon?.icon?.name;
+        const isIconVisible = isIcon && hasValidIconName;
+
+        const cardBackgroundSrc = getCardBackgroundImageSrc(card.groupCardBackground?.image);
+        const hasCardBackgroundImage = Boolean(cardBackgroundSrc);
+
+        const cardClasses = cx('hs-elevate-service-card-container__card', styles[`hs-elevate-service-card-container__card--${cardOrientation}`], {
+          [styles['hs-elevate-service-card-container__card--no-button']]: !showButton,
+          [styles['hs-elevate-service-card-container__card--bg-image']]: hasCardBackgroundImage,
+          [styles['hs-elevate-service-card-container__card--drop-shadow']]: showCardShadow,
+        });
+
+        const cardSurfaceStyles: CSSPropertiesMap | undefined = (() => {
+          const stylesMap: CSSPropertiesMap = {};
+          if (hasCardBackgroundImage) {
+            stylesMap.backgroundImage = `url(${cardBackgroundSrc})`;
+          }
+          if (showCardBorder === false) {
+            stylesMap.border = 'none';
+          }
+          return Object.keys(stylesMap).length ? stylesMap : undefined;
+        })();
+
+        const showRoundImageBorder = card.groupImage.showRoundImageBorder === true;
+
+        const imageWrapperClasses = cx(swm('hs-elevate-service-card-container__image-wrapper'), {
+          [styles['hs-elevate-service-card-container__image-wrapper--use-background']]: cardImageUsesBackground,
+          [styles['hs-elevate-service-card-container__image-wrapper--round-border']]:
+            isImageVisible && showRoundImageBorder,
+        });
+
+        return (
+          <Card
+            additionalClassArray={[cardClasses]}
+            key={index}
+            cardStyleVariant={cardStyleVariant}
+            cardOrientation={cardOrientation}
+            inlineStyles={cardSurfaceStyles}
+          >
+            {isIconVisible && (
+              <IconWrapper className={swm('hs-elevate-service-card-container__icon-wrapper')}>
+                <Icon
+                  className={cx(swm('hs-elevate-service-card-container__icon'), {
+                    [styles['hs-elevate-service-card-container__icon--no-frame']]: !showIconBorder,
+                  })}
+                  purpose="DECORATIVE"
+                  fieldPath={`groupCards[${index}].groupIcon.icon`}
+                />
+              </IconWrapper>
+            )}
+            {isImageVisible && (
+              <ImageWrapper className={cx(imageWrapperClasses)}>
+                <Image
+                  className={swm('hs-elevate-service-card-container__image')}
+                  src={card.groupImage.image.src}
+                  alt={card.groupImage.image.alt}
+                  width={card.groupImage.image.width}
+                  height={card.groupImage.image.height}
+                  loading={card.groupImage.image.loading !== 'disabled' ? card.groupImage.image.loading : 'eager'}
+                  data-hs-token={getDataHSToken(moduleName, `groupCards[${index}].groupImage.image`)}
+                />
+              </ImageWrapper>
+            )}
+            <CardContent className={swm('hs-elevate-service-card-container__content')}>
+              {card.groupContent.showCaption === true && Boolean(card.groupContent.captionText?.trim()) && (
+                <Caption
+                  className={swm('hs-elevate-service-card-container__caption')}
+                  style={{ textAlign: textAlignment }}
+                  data-hs-token={getDataHSToken(moduleName, `groupCards[${index}].groupContent.captionText`)}
+                >
+                  {card.groupContent.captionText}
+                </Caption>
+              )}
+              {card.groupContent.headingAndTextHeading && (
+                <HeadingComponent
+                  headingLevel={card.groupContent.headingAndTextHeadingLevel}
+                  heading={card.groupContent.headingAndTextHeading}
+                  headingStyleVariant={headingStyleVariant}
+                  inlineStyles={headingInlineStyles}
+                  headingUppercase={headingUppercase}
+                  additionalClassArray={[swm('hs-elevate-service-card-container__title')]}
+                  moduleName={moduleName}
+                  fieldPath={`groupCards[${index}].groupContent.headingAndTextHeading`}
+                />
+              )}
+              <RichText
+                fieldPath={`groupCards[${index}].groupContent.richTextContentHTML`}
+                className={swm('hs-elevate-service-card-container__body')}
+                data-hs-token={getDataHSToken(moduleName, `groupCards[${index}].groupContent.richTextContentHTML`)}
+              />
+              {showButton && (
+                <ButtonWrapper className={swm('hs-elevate-service-card-container__button-wrapper')}>
+                  <Button
+                    buttonSize={buttonStyleSize}
+                    buttonStyle={buttonStyleVariant}
+                    href={getLinkFieldHref(link)}
+                    rel={getLinkFieldRel(link)}
+                    target={getLinkFieldTarget(link)}
+                    iconFieldPath={`groupCards[${index}].groupButton.buttonContentIcon`}
+                    showIcon={showIcon}
+                    iconPosition={iconPosition}
+                    additionalClassArray={['hs-elevate-service-card-container__button']}
+                    moduleName={moduleName}
+                    textFieldPath={`groupCards[${index}].groupButton.buttonContentText`}
+                  >
+                    {text}
+                  </Button>
+                </ButtonWrapper>
+              )}
+            </CardContent>
+          </Card>
+        );
+      })}
+    </CardContainer>
+  );
+};
+
+export { fields } from './fields.js';
+
+export const hublDataTemplate = `
+  {% set hublData = {
+      "renderedWithGrids": rendered_with_grids,
+    }
+  %}
+`;
+
+export const meta: ModuleMeta = {
+  label: 'ServiceCard',
+  content_types: ['BLOG_LISTING', 'BLOG_POST', 'SITE_PAGE', 'LANDING_PAGE', 'CASE_STUDY'],
+  icon: cardIconSvg,
+  categories: ['design'],
+};
+
+export const defaultModuleConfig = {
+  moduleName: 'elevate/components/modules/service_card',
+  version: 0,
+  themeModule: true,
+};
+
